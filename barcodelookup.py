72efcc93ae0ff224ecae62fa10a817448687f44f
Rendy <zrendy@gmail.com>
2019-04-10 12:59:37 -0400

    hi

--- barcodelookup.py
+++ barcodelookup.py
@@ -3,7 +3,7 @@
 #sudo apt-get install software-properties-common
 #sudo apt-get install python3-bs4
 
-
+import urllib
 from urllib.request import urlopen as req
 from bs4 import BeautifulSoup as soup
 import datetime
@@ -13,7 +13,8 @@ url="https://www.upcdatabase.com/item/"
 while True:
     upc=input()
     global lookup_url
-    lookup_url=url+upc    
+    lookup_url=url+upc
+    
     def scraper():
         response=req(lookup_url)
         html_page=response.read()
@@ -25,13 +26,22 @@ while True:
         if (check=="Item Not Found") or (check=="UPC Error"):
             return
         #scrape based on tags
-        itemdescription=soupypage.find("table",{"class":"data"}).findAll("tr")[2].findAll("td")[2].text
-        sizeweight=soupypage.find("table",{"class":"data"}).findAll("tr")[3].findAll("td")[2].text
-        country=soupypage.find("table",{"class":"data"}).findAll("tr")[4].findAll("td")[2].text
+        itemdescription3=str(soupypage.find("table",{"class":"data"}).findAll("tr")[3].findAll("td"))
+        itemdescription2=str(soupypage.find("table",{"class":"data"}).findAll("tr")[2].findAll("td"))
+    
+        
+        if (itemdescription3.find("Description",0,len(itemdescription3)))!=-1:
+            itemdescription=soupypage.find("table",{"class":"data"}).findAll("tr")[3].findAll("td")[2].text
+            print("hi")
+        elif (itemdescription2.find("Description",0,len(itemdescription2)))!=-1:
+            itemdescription=soupypage.find("table",{"class":"data"}).findAll("tr")[2].findAll("td")[2].text
+            print("he")
+        else:
+            itemdescription=soupypage.find("table",{"class":"data"}).findAll("tr")[1].findAll("td")[2].text
+            print("ho")
+        
         print("UPC:" +upc)
         print("The item is: " + itemdescription)
-        print("Size and weight of: " + sizeweight)
-        print("From country: " +country)
         now = datetime.datetime.now()
         print ("Current date and time : " + now.strftime("%Y-%m-%d %H:%M:%S"))
     scraper()
